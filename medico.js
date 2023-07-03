aaec977a27bf701a43e89843f36929d27879de1e
MACARENASTORE <oclusiva9204@gmail.com>
2023-07-03 18:16:58 -0500

    version tallerprueba

--- medico.js
+++ medico.js
@@ -1,121 +1,45 @@
-class FormEngine {
-  async medico() {
-    const response = await fetch('http://localhost:3000/formulario/Medico');
-    const form = await response.json();
+document.addEventListener('DOMContentLoaded', () => {
+  const medicoForm = document.getElementById('medicoForm');
+  medicoForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (medicoForm.checkValidity()) {
+      const tarjetaProfesional = document.getElementById('tarjetaProfesional').value;
+      const nombre = document.getElementById('nombre').value;
+      const apellido = document.getElementById('apellido').value;
+      const consultorio = document.getElementById('consultorio').value;
+      const correo = document.getElementById('correo').value;
+      const idEspecialidad = document.getElementById('idEspecialidad').value;
 
-    const formContainer = document.createElement('div');
-    formContainer.classList.add('container');
+      const formData = {
+        tarjetaProfesional,
+        nombre,
+        apellido,
+        consultorio,
+        correo,
+        idEspecialidad
+      };
 
-    const formElement = document.createElement('form');
-    formElement.classList.add('form');
+      try {
+        const response = await fetch('http://localhost:3001/medicos', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(formData)
+        });
 
-    for (const [key, value] of Object.entries(form.properties)) {
-      const label = document.createElement('label');
-      label.innerText = key;
-      formElement.appendChild(label);
-
-      const inputContainer = document.createElement('div');
-      inputContainer.classList.add('form-input-container');
-
-      const input = document.createElement('input');
-      input.name = key;
-      input.classList.add('form-control');
-
-      if (value.type === 'integer') {
-        input.type = 'number';
-      } else {
-        input.type = 'text';
+        if (response.ok) {
+          document.getElementById('message').textContent = 'Datos enviados correctamente.';
+          medicoForm.reset();
+        } else {
+          document.getElementById('message').textContent = 'Error al enviar los datos.';
+        }
+      } catch (error) {
+        console.error(error);
+        document.getElementById('message').textContent = 'Error al enviar los datos.';
       }
-
-      inputContainer.appendChild(input);
-      formElement.appendChild(inputContainer);
     }
-
-    const submitButton = document.createElement('button');
-    submitButton.type = 'submit';
-    submitButton.classList.add('btn', 'btn-primary');
-    submitButton.innerHTML = '<i class="bi bi-cloud-arrow-up-fill me-2"></i>Enviar';
-    formElement.appendChild(submitButton);
-
-    const messageElement = document.createElement('div');
-    messageElement.classList.add('message');
-
-    const updateButton = document.createElement('button');
-    updateButton.type = 'button';
-    updateButton.classList.add('btn', 'btn-secondary');
-    updateButton.innerHTML = '<i class="bi bi-pencil-fill me-2"></i>Actualizar';
-    formElement.appendChild(updateButton);
-
-    const deleteButton = document.createElement('button');
-    deleteButton.type = 'button';
-    deleteButton.classList.add('btn', 'btn-danger');
-    deleteButton.innerHTML = '<i class="bi bi-trash-fill me-2"></i>Eliminar';
-    formElement.appendChild(deleteButton);
-
-    formElement.addEventListener('submit', async (event) => {
-      event.preventDefault();
-
-      const formData = new FormData(event.target);
-      const formValues = Object.fromEntries(formData.entries());
-
-      const response = await fetch('http://localhost:3000/crear_medico', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formValues)
-      });
-
-      if (response.ok) {
-        messageElement.textContent = 'Datos enviados Gracias';
-      } else {
-        messageElement.textContent = 'Error al enviar los datos ';
-      }
-    });
-
-    updateButton.addEventListener('click', async () => {
-      const formData = new FormData(formElement);
-      const formValues = Object.fromEntries(formData.entries());
-
-      const response = await fetch('http://localhost:3000/actualizar_medico/tarjetaProfesional', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formValues)
-      });
-
-      if (response.ok) {
-        messageElement.textContent = 'Datos actualizados ';
-      } else {
-        messageElement.textContent = 'Error al actualizar los datos ';
-      }
-    });
-
-    deleteButton.addEventListener('click', async () => {
-      const formData = new FormData(formElement);
-      const formValues = Object.fromEntries(formData.entries());
-
-      const response = await fetch('http://localhost:3000/eliminar_medico/tarjetaProfesional', {
-        method: 'DELETE',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(formValues)
-      });
-
-      if (response.ok) {
-        messageElement.textContent = 'Datos eliminados ';
-      } else {
-        messageElement.textContent = 'Error al eliminar los datos ';
-      }
-    });
-
-    formContainer.appendChild(formElement);
-    formContainer.appendChild(messageElement);
-    document.body.appendChild(formContainer);
-  }
-}
-
-const miFormEngine = new FormEngine();
-miFormEngine.medico();
+    medicoForm.classList.add('was-validated');
+  });
+});
--- paciente.js
+++ paciente.js
@@ -1,68 +1,43 @@
-class FormEngine {
-    async paciente() {
-      const response = await fetch('http://localhost:3001/formulario/Paciente');
-      const form = await response.json();
-  
-      const formContainer = document.createElement('div');
-      formContainer.classList.add('container');
-  
-      const formElement = document.createElement('form');
-      formElement.classList.add('form');
-  
-      for (const [key, value] of Object.entries(form.properties)) {
-        const label = document.createElement('label');
-        label.innerText = key;
-        formElement.appendChild(label);
-  
-        const inputContainer = document.createElement('div');
-        inputContainer.classList.add('form-input-container');
-  
-        const input = document.createElement('input');
-        input.name = key;
-        input.classList.add('form-control');
-  
-        if (value.type === 'integer') {
-          input.type = 'number';
-        } else {
-          input.type = 'text';
-        }
-  
-        inputContainer.appendChild(input);
-        formElement.appendChild(inputContainer);
-      }
-  
-      const submitButton = document.createElement('button');
-      submitButton.type = 'submit';
-      submitButton.classList.add('btn', 'btn-primary');
-      submitButton.innerHTML = '<i class="bi bi-cloud-arrow-up-fill me-2"></i>Enviar';
-      formElement.appendChild(submitButton);
-  
-      formElement.addEventListener('submit', async (event) => {
-        event.preventDefault();
-  
-        const formData = new FormData(event.target);
-        const formValues = Object.fromEntries(formData.entries());
-  
-        const response = await fetch('http://localhost:3001/crear_paciente', {
+document.addEventListener('DOMContentLoaded', () => {
+  const pacienteForm = document.getElementById('pacienteForm');
+  pacienteForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    if (pacienteForm.checkValidity()) {
+      const cedula = document.getElementById('cedula').value;
+      const nombre = document.getElementById('nombre').value;
+      const apellido = document.getElementById('apellido').value;
+      const fecha = document.getElementById('fecha').value;
+      const telefono = document.getElementById('telefono').value;
+
+      const formData = {
+        cedula,
+        nombre,
+        apellido,
+        fecha,
+        telefono
+      };
+
+      try {
+        const response = await fetch('http://localhost:3001/pacientes', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
           },
-          body: JSON.stringify(formValues)
+          body: JSON.stringify(formData)
         });
-  
+
         if (response.ok) {
-          console.log('Datos enviados a la base de datos');
+          document.getElementById('message').textContent = 'Datos enviados correctamente.';
+          pacienteForm.reset();
         } else {
-          console.error('Error al enviar los datos a la base de datos');
+          document.getElementById('message').textContent = 'Error al enviar los datos.';
         }
-      });
-  
-      formContainer.appendChild(formElement);
-      document.body.appendChild(formContainer);
+      } catch (error) {
+        console.error(error);
+        document.getElementById('message').textContent = 'Error al enviar los datos.';
+      }
     }
-  }
-  
-  const miFormEngine = new FormEngine();
-  miFormEngine.paciente();
-  
+    pacienteForm.classList.add('was-validated');
+  });
+});
